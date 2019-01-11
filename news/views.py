d2fba1a22b8e0c7b0995e22f35be3ec13e3b93e1
cz9025 <897308902@qq.com>
2019-01-11 21:26:23 +0800

    news

--- news/views.py
+++ news/views.py
@@ -7,6 +7,7 @@ from . import models
 from concurrent.futures import ThreadPoolExecutor
 import time
 from pyquery import PyQuery
+import requests
 
 p = ThreadPoolExecutor(10)
 
@@ -17,15 +18,19 @@ def index(request):
                       "Safari/537.36",
     }
     banana = []
-    licai = []
+    people = []
     paper_title = []
     baidu_title = []
     car_title = []
 
     def get_pyquery(url):
-        u"""通用请求"""
-        pq = PyQuery(url=url, headers=headers, encoding='utf-8')
-        return pq
+        u"""通用请求  , encoding='utf-8'"""
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            pq = PyQuery(response.content)
+            return pq
+        else:
+            return 0
 
     def hq_banana(res):
         """banana图"""
@@ -51,23 +56,32 @@ def index(request):
                 banana.append(mydata)
 
     def hx_money(res):
-        """和讯理财"""
+        """人民网"""
         pq = res.result()
+        # pq = PyQuery(div)
 
+        headingNews = pq('.headingNews_box .headingNews:first').find(".hdNews")
+        for news in headingNews.items():
+            a = news.find("strong a")
+            if a:
+                a_title = a.text()
+                a_href ="http://house.people.com.cn" + a.attr("href")
+                gray = news.find("p a").text()
 
-        pass
-
-
-
-
+                mydata = {
+                    "a_title": a_title,
+                    "a_href": a_href,
+                    "gray": gray
+                }
+                print mydata
+                people.append(mydata)
 
     with ThreadPoolExecutor(10) as p:
-
         # 轮播图
         p.submit(get_pyquery, "http://world.huanqiu.com").add_done_callback(hq_banana)
         # 和讯理财
-        p.submit(get_pyquery, "http://money.hexun.com").add_done_callback(hx_money)
+        p.submit(get_pyquery, "http://house.people.com.cn").add_done_callback(hx_money)
 
     # time.sleep(1)
 
-    return render(request, 'news/news.html', {"banana": banana})
+    return render(request, 'news/news.html', {"banana": banana, "people": people})
