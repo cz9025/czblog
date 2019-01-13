5541751e3d5de3e5de5b1a27dcb5b26fdc5bc5e9
cz9025 <897308902@qq.com>
2019-01-13 18:38:36 +0800

    准备做置顶

--- static/js/blog.js
+++ static/js/blog.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
         $(this).attr('href','?page='+nu);
 
     });
-	
+
+    // 置顶功能  这个要重新做，这里只是调试
+	$(".feedlist_id>li:first>h2").append("<span></span>");
 		
 });
