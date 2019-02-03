9c5a7cd1f53ca40c1d0cb024d1790868e616e844
cz9025 <897308902@qq.com>
2019-02-03 11:50:43 +0800

    bug

--- center/adminx.py
+++ center/adminx.py
@@ -3,7 +3,7 @@
 # @Author  : chengz
 # @File    : adminx.py
 # @Software: PyCharm
-from xadmin import views
+
 from .models import UserInfo
 import xadmin
 
@@ -11,8 +11,19 @@ import xadmin
 class UserInfoAdmin(object):
     list_display = ('id', 'username', 'nick_name', 'birthday', 'gender', 'mobile', 'email', 'address', 'last_login')
     search_fields = ('username',)
+    list_per_page = 20
 
 
 xadmin.site.unregister(UserInfo)
 xadmin.site.register(UserInfo, UserInfoAdmin)
 
+
+# 图表   没起作用
+class ReporDataAdmin(object):
+    list_display = ('year', 'cn')
+    list_per_page = 20
+    data_charts = {
+        "user_count": {'title': u"User Register Raise", "x-field": "day", "y-field": ("cn",),
+                       # "order": ('day',)
+                       },
+    }
